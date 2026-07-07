fa10c8e1af73f9d0c27d74a8a5c3f5bc3846ecb5
drmatson <margarita.matson@taltech.ee>
2026-07-07 11:59:22 +0300

    s letter update

--- 4tronix/scripts/install_4tronix_mirror.sh
+++ 4tronix/scripts/install_4tronix_mirror.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # M.A.R.S. Rover software installer
-# Marsy / AstroSheeps mirror version
+# Marsy / AstroSheep mirror version
 #
 # New Marsy repository structure:
 #   4tronix/original/             - original 4tronix rover files
@@ -12,11 +12,11 @@
 # needed on the Raspberry Pi / real rover.
 #
 # Source mirror:
-# https://github.com/astro-sheeps/marsy/tree/main/4tronix/original
+# https://github.com/astro-sheep/marsy/tree/main/4tronix/original
 
 set -e
 
-BASE_URL="https://raw.githubusercontent.com/astro-sheeps/marsy/main/4tronix/original"
+BASE_URL="https://raw.githubusercontent.com/astro-sheep/marsy/main/4tronix/original"
 TARGET_DIR="$HOME/marsrover"
 
 FILES=(
@@ -35,7 +35,7 @@ echo "Creating marsrover folder"
 mkdir -p "$TARGET_DIR"
 cd "$TARGET_DIR"
 
-echo "Copying M.A.R.S. Rover files from AstroSheeps mirror"
+echo "Copying M.A.R.S. Rover files from AstroSheep mirror"
 echo "Source: $BASE_URL"
 echo "Target: $TARGET_DIR"
 
